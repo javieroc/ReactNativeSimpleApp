5b73a94d1e09c51a5f4651aba0eb75f4bf6db0de
Javier Ocampo <ferroxido@gmail.com>
2018-08-31 17:37:37 -0300

    Add simple FlatList

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text } from 'react-native';
 import Home from './src/screens/containers/Home';
 import Header from './src/sections/components/Header';
+import SuggestionList from './src/videos/containers/SuggestionList';
 
 type Props = {};
 
@@ -12,7 +13,7 @@ export default class App extends Component<Props> {
         <Header />
         <Text>Buscador</Text>
         <Text>Categorias</Text>
-        <Text>Sugerencias</Text>
+        <SuggestionList />
       </Home>
     );
   }
